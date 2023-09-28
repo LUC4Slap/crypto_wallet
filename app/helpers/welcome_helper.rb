d59f62f66449eb0725f0e2dbcad66ee9fd5cbe95
LUC4Slap <lucaslap27@gmail.com>
2023-09-28 17:34:33 -0400

    feat: adiciona parametro para poder passar a moeda que queira pegar a cotação

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,11 +1,11 @@
 require 'net/http'
 require "json"
 module WelcomeHelper
-    def btc_price_US
+    def btc_price(nome_moeda)
       lista = Net::HTTP.new('blockchain.info', 443)
       lista.use_ssl = true
       response = lista.get('/ticker')
       date = JSON.parse(response.body)
-      date['USD']['buy']
+      date[nome_moeda]['buy']
     end
 end
